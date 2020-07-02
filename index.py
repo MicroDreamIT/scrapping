c198731b2131195ccc9702a956699429bbd4a294
sharif <shahanur700@gmail.com>
2020-07-03 05:43:18 +0600

    first

---
+++ index.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+
+browser = webdriver.Chrome()
+browser.get("file:///Users/shahanurmdsharif/development/python/scrapping/scrap.html")
