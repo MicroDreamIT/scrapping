042fe2b37c721ddc8bb7527cc92998c11a0dec3f
sharif <shahanur700@gmail.com>
2020-07-03 16:36:04 +0600

    first

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
+import os
 
 browser = webdriver.Chrome()
-browser.get("file:///Users/shahanurmdsharif/development/python/scrapping/scrap.html")
+browser.get("https://www.google.com")
+# browser.get("file:///Users/shahanurmdsharif/development/python/scrapping/scrap.html")
