6b84f6e5b65b48c1fbdabc479c90ef3837a93ef1
sharif <shahanur700@gmail.com>
2020-07-04 02:59:08 +0600

    first

--- index.py
+++ index.py
@@ -3,6 +3,25 @@ from selenium import webdriver
 import time
 import os
 
-browser = webdriver.Chrome()
-browser.get("https://www.google.com")
-# browser.get("file:///Users/shahanurmdsharif/development/python/scrapping/scrap.html")
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+show_browser = 0
+if show_browser:
+    browser = webdriver.Chrome()
+else:
+    browser = webdriver.Chrome(options=options)
+
+
+browser.get("file:///Users/shahanurmdsharif/development/python/scrapping/scrap.html")
+time.sleep(2)
+html = browser.page_source
+
+source = BeautifulSoup(html, features="html.parser")
+
+forms = source.find_all('form')
+
+print(forms[9])
+
+
+browser.close()
